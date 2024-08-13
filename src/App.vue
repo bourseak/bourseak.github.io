330ca69ed1f10ba463431d7521d6c80ebe6f96bb
shabane <m.mohamadshabane@gmail.com>
2024-08-13 16:48:44 +0330

    add navebar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div style="background-color: #313131; height: 100vh">
+    <NavbarCmp />
+
+    <div class="container">
+      <router-view />
+    </div>
+  </div>
 </template>
 
 <style>
@@ -12,19 +14,15 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
+
+<script>
+import NavbarCmp from "@/components/NavbarCmp.vue";
+
+export default {
+  components: {
+    NavbarCmp,
+  },
+};
+</script>
---
+++ src/views/LoginForm.vue
@@ -0,0 +1,23 @@
+<script setup></script>
+
+<template>
+  <div dir="rtl" class="my-border row p-5 w-25">
+    <input
+      class="my-input interface m-2 p-3"
+      type="text"
+      placeholder="شماره مبایل"
+    />
+    <input
+      class="my-input interface m-2 p-3"
+      type="password"
+      placeholder="رمز عبور"
+    />
+    <input
+      class="m-2 p-3 w-25 interface m-1 my-border my-btn"
+      type="submit"
+      value="ورود"
+    />
+  </div>
+</template>
+
+<style scoped></style>
