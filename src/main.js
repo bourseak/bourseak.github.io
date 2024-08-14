9a6b0bd4fc8cc4ddf5ddf96d1e515cfa03a14f9b
shabane <m.mohamadshabane@gmail.com>
2024-08-14 16:34:42 +0330

    feat: create a global function to get cookie by name.

--- src/main.js
+++ src/main.js
@@ -5,4 +5,20 @@ import "./assets/style.css";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-createApp(App).use(router).mount("#app");
+function get_cookie(key) {
+  const cookies = document.cookie.split(";");
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i].trim();
+    if (cookie.startsWith(key)) {
+      return cookie.substring(key.length + 1);
+    }
+  }
+  return null;
+}
+
+const app = createApp(App);
+app.config.globalProperties.$get_cookie = get_cookie;
+app.use(router);
+app.mount("#app");
+
+// createApp(App).use(router).mount("#app");
