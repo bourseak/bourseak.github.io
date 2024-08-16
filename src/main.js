5b918d2aa14d32bf11180a538df64cdba2a2beb0
shabane <m.mohamadshabane@gmail.com>
2024-08-16 15:04:24 +0330

    chore: install axios and import

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import VueSweetalert2 from "vue-sweetalert2";
 import "sweetalert2/dist/sweetalert2.min.css";
+import axios from "axios";
 
 function get_cookie(key) {
   const cookies = document.cookie.split(";");
