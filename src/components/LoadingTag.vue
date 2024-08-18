f237122ae69c31eb4c25bd25c99552b35c86b6e8
shabane <m.mohamadshabane@gmail.com>
2024-08-18 04:33:20 +0330

    feat: component: create a loading component

---
+++ src/components/LoadingTag.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="spinner-grow p-color" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+    <div class="spinner-grow g-color" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+    <div class="spinner-grow b-color" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+    <div class="spinner-grow o-color" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped></style>
