a3df091c5744cfc8b47d35524a1e60c86345c2f8
shabane <m.mohamadshabane@gmail.com>
2024-08-29 04:29:25 +0330

    feat: create warning performance

---
+++ src/components/WarningPerformance.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container row justify-content-center p-3">
+    <div>
+      <button
+        class="my-btn interface my-border col-xl-2 col-10 p-2 text-center"
+      >
+        اتضال به تلگرام
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+a {
+  text-decoration: none;
+}
+</style>
+
+<script>
+export default {
+  name: "WarningPerformance",
+};
+</script>
