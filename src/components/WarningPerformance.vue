b20279f0beadd4bd053c45400cc45a454b0c7d57
shabane <m.mohamadshabane@gmail.com>
2024-08-29 04:53:03 +0330

    feat: now user can login to telegram using the button;

--- src/components/WarningPerformance.vue
+++ src/components/WarningPerformance.vue
@@ -3,6 +3,7 @@
     <div>
       <button
         class="my-btn interface my-border col-xl-2 col-10 p-2 text-center"
+        @click="connect_tel"
       >
         اتضال به تلگرام
       </button>
@@ -17,7 +18,28 @@ a {
 </style>
 
 <script>
+import axios from "axios";
+import Swal from "sweetalert2";
+
 export default {
   name: "WarningPerformance",
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {
+    async connect_tel() {
+      var token = await axios.get(`${this.$host}/api/telath/`, this.$config);
+      Swal.fire({
+        icon: "warning",
+        title: "برای اتصال به تلگرام باید فیلترشکن شما روشن باشد",
+        confirmButtonText: "متوجه شدم",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          window.open(token.data.auth_link);
+        }
+      });
+    },
+  },
 };
 </script>
