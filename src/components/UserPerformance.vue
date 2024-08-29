73b2ada054b0813cf2ae586a87242ee96995a38b
shabane <m.mohamadshabane@gmail.com>
2024-08-29 03:50:21 +0330

    feat: create just a from for user details

--- src/components/UserPerformance.vue
+++ src/components/UserPerformance.vue
@@ -1,6 +1,53 @@
 <template>
   <div>
     <div class="container row align-content-center justify-content-center p-3">
+      <div class="row col-12 col-lg-3">
+        <div class="row align-items-center">
+          <input
+            type="text"
+            class="user-input interface text-center m-1 col-10"
+            disabled
+            :value="user.phone"
+          />
+          <label class="col-1">شماره</label>
+        </div>
+
+        <div class="row align-items-center">
+          <input
+            type="text"
+            class="user-input interface text-center m-1 col-10"
+            disabled
+            :value="user.email"
+          />
+          <label class="col-1">ایمیل</label>
+        </div>
+
+        <div class="row align-items-center">
+          <input
+            type="text"
+            class="user-input interface text-center m-1 col-10"
+            v-model="user.first_name"
+          />
+          <label class="col-1">نام</label>
+        </div>
+
+        <div class="row align-items-center">
+          <input
+            type="text"
+            class="interface text-center user-input m-1 col-10"
+            v-model="user.last_name"
+          />
+          <label class="col-1">نام خانوادگی</label>
+        </div>
+
+        <div class="row align-items-center mt-3">
+          <button class="my-border my-btn interface m-1 p-2 col-10">
+            ذخیره تغییرات
+          </button>
+        </div>
+      </div>
+      <hr class="m-5" />
+
       <button class="my-btn interface my-border w-25 p-2" @click="logout">
         خروج از حساب کاربری
       </button>
@@ -9,15 +56,29 @@
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.user-input {
+  border: 0px;
+  border-radius: 13px;
+  background-color: #404040;
+  height: 40px;
+}
+</style>
 
 <script>
 import Swal from "sweetalert2";
+import axios from "axios";
 
 export default {
   name: "UserPerformance",
-  data() {},
-  mounted() {},
+  data() {
+    return {
+      user: {},
+    };
+  },
+  mounted() {
+    this.get_user();
+  },
   methods: {
     logout() {
       Swal.fire({
@@ -36,6 +97,17 @@ export default {
           Swal.fire("Changes are not saved", "", "info");
         }
       });
+    },
+    get_user() {
+      if (this.$is_logedin) {
+        axios
+          .get(`${this.$host}/api/user/`, this.$config)
+          .then((data) => {
+            console.log(data.data);
+            this.user = data.data;
+          })
+          .catch((err) => console.log(err));
+      }
     },
   },
 };
