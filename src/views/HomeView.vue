8b9d86bccc28b2ca9a93b03326559a7211529efa
shabane <m.mohamadshabane@gmail.com>
2025-02-21 01:31:16 +0330

    feat: update home with all plans

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,82 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="row row-cols-1 row-cols-md-3 g-4 mt-5 mb-5">
+      <div class="col">
+        <div class="card h-100">
+          <img src="@/assets/r2d2.jpeg" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">R2D2</h5>
+            <p class="card-text" dir="rtl">
+              پلن R2D2 کاملا رایگانه. شما با این پنل می‌تونید تا حد اقل
+              <b>3 سهم</b>
+              رو به واچ لیست اضافه کنید
+            </p>
+          </div>
+          <div class="card-footer">
+            <small class="text-body-secondary" dir="rtl">رایگانه (:</small>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card h-100">
+          <img src="@/assets/C-3PO.jpeg" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">C-3PO</h5>
+            <p class="card-text" dir="rtl">
+              پلن C-3PO. شما با این پنل می‌تونید تا حد اقل
+              <b>5 سهم</b>
+              رو به واچ لیست اضافه کنید
+            </p>
+          </div>
+          <div class="card-footer">
+            <small class="text-body-secondary" dir="rtl">25,000 تومان</small>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card h-100">
+          <img src="@/assets/bb8.jpeg" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">BB-8</h5>
+            <p class="card-text" dir="rtl">
+              پلن BB-8. شما با این پنل می‌تونید تا حد اقل
+              <b>10 سهم</b>
+              رو به واچ لیست اضافه کنید
+            </p>
+          </div>
+          <div class="card-footer">
+            <small class="text-body-secondary" dir="rtl">50,000 تومان</small>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card h-100">
+          <img src="@/assets/K-2SO.jpeg" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">K-2SO</h5>
+            <p class="card-text" dir="rtl">
+              پلن K-2SO. شما با این پنل می‌تونید تا حد اقل
+              <b>20 سهم</b>
+              رو به واچ لیست اضافه کنید
+            </p>
+          </div>
+          <div class="card-footer">
+            <small class="text-body-secondary" dir="rtl">90,000 تومان</small>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
