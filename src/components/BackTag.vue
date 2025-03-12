88ea23665f5b6da2e4649843f37e8618fc2b40e6
shabane <m.mohamadshabane@gmail.com>
2025-03-12 22:25:15 +0330

    feat: create a BackTag that can back to prob

---
+++ src/components/BackTag.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button class="btn btn-outline-light" @click="back()">
+      <span class="icono-caretLeft"></span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BackTag",
+  props: {
+    backTo: String,
+  },
+  methods: {
+    back() {
+      this.$router.push(this.backTo);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
