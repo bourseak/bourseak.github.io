1ad4db23a951e0bf81b9d1db84e503dc3c1053a3
shabane <m.mohamadshabane@gmail.com>
2025-04-05 13:24:06 +0330

    maintain: add viedo toturial and add some text explains

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
-    <div class="row row-cols-1 row-cols-md-3 g-4 mt-5 mb-5">
+    <!--    dont show the plans-->
+    <div
+      class="row row-cols-1 row-cols-md-3 g-4 mt-5 mb-5"
+      style="display: none"
+    >
       <div class="col">
         <div class="card h-100">
           <img src="@/assets/r2d2.jpeg" class="card-img-top" alt="..." />
@@ -65,6 +69,81 @@
           </div>
           <div class="card-footer">
             <small class="text-body-secondary" dir="rtl">90,000 تومان</small>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <p style="direction: rtl; font-size: 25px" class="w-color m-5">
+      هدف ما از ارائه این سایت کمک به شما در خرید و فروش نماد های بورسی به قیمت
+      های مناسب می‌باشد. که این امر فعلا به دو شکل برای شما فرهم است.
+      <br />
+      <br />
+      1. رسیدن قیمت نماد به قیمتی که شما تعیین می‌کنید برای خرید و یا فروش سهم.
+      <br />
+      2. حجم خوردن نماد به شکل غیر متعارف که اخطاری برای خرید و یا فروش سهم
+      می‌باشد.
+    </p>
+    <hr class="p-color" />
+    <div dir="rtl" class="mt-5 w-color mb-5">
+      <h3>آموزش ها</h3>
+
+      <div class="row row-cols-1 row-cols-md-2 g-4 mt-1">
+        <div class="col">
+          <div class="card">
+            <video class="card-img-top" controls>
+              <source
+                src="https://raw.githubusercontent.com/bit-orbit/CDN/refs/heads/master/files/documents/add.mp4"
+                type="video/mp4"
+              />
+            </video>
+            <div class="card-body">
+              <h5 class="card-title">اضافه کردن یک سهم به واچ لیست</h5>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card">
+            <video class="card-img-top" controls>
+              <source
+                src="https://raw.githubusercontent.com/bit-orbit/CDN/refs/heads/master/files/documents/addVol.mp4"
+                type="video/mp4"
+              />
+            </video>
+            <div class="card-body">
+              <h5 class="card-title">
+                اضافه کردن سهم بر اساس حجم مشکوک ماهانه
+              </h5>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card">
+            <video class="card-img-top" controls>
+              <source
+                src="https://raw.githubusercontent.com/bit-orbit/CDN/refs/heads/master/files/documents/edit.mp4"
+                type="video/mp4"
+              />
+            </video>
+            <div class="card-body">
+              <h5 class="card-title">ویرایش یک سهم از واچ لیست</h5>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card">
+            <video class="card-img-top" controls>
+              <source
+                src="https://raw.githubusercontent.com/bit-orbit/CDN/refs/heads/master/files/documents/notiftel.mp4"
+                type="video/mp4"
+              />
+            </video>
+            <div class="card-body">
+              <h5 class="card-title">اتصال به تلگرام برای دریافت اخطار‌ها</h5>
+            </div>
           </div>
         </div>
       </div>
